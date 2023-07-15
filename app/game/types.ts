20ab21af19cfa99bd71f72b3f54a59ae5a2b6295
Jordan Paris <hamologist@gmail.com>
2023-07-15 00:32:28 -0400

    Adds local multiplayer game state checking logic

--- app/game/types.ts
+++ app/game/types.ts
@@ -1,13 +1,14 @@
 export type GamePieceStates = 'circle' | 'cross' | 'none';
 export type BoardRow = [GamePieceStates, GamePieceStates, GamePieceStates];
 export type BoardState = [BoardRow, BoardRow, BoardRow];
+export type ValidPositionIndex = 0 | 1 | 2;
 
 export type BoardSquareContext = [GamePieceStates, () => void];
 export type BoardSquareRowContext = [BoardSquareContext, BoardSquareContext, BoardSquareContext];
 export type BoardContext = [BoardSquareRowContext, BoardSquareRowContext, BoardSquareRowContext];
 
 export type CurrentPlayer = 'PlayerOne' | 'PlayerTwo';
-export type ValidPositionIndex = 0 | 1 | 2;
+export type GameStates = 'PlayerOneWin' | 'PlayerTwoWin' | 'Draw' | 'Playing' | 'Processing';
 
 export type UseGameManager = {
   readonly boardState: BoardState;
--- app/game/use-local-game-manager.ts
+++ app/game/use-local-game-manager.ts
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import generateBoardContext from './board-context';
 import {
   UseGameManager,
@@ -7,6 +7,7 @@ import {
   CurrentPlayer,
   ValidPositionIndex,
   BoardSquareContext,
+  GameStates,
 } from './types';
 
 export default function useLocalGameManager(): UseGameManager {
@@ -16,13 +17,14 @@ export default function useLocalGameManager(): UseGameManager {
     ['none', 'none', 'none'],
   ]);
   const [currentPlayer, setCurrentPlayer] = useState<CurrentPlayer>('PlayerOne');
+  const [gameState, setGameState] = useState<GameStates>('Playing');
 
   const generateBoardSquareClickHandler = (
     rowIndex: ValidPositionIndex,
     colIndex: ValidPositionIndex,
   ): BoardSquareContext[1] => {
     return () => {
-      if (boardState[rowIndex][colIndex] === 'none') {
+      if (gameState === 'Playing' && boardState[rowIndex][colIndex] === 'none') {
         const newBoardState = [
           [...boardState[0]],
           [...boardState[1]],
@@ -34,16 +36,76 @@ export default function useLocalGameManager(): UseGameManager {
         } else {
           newBoardState[rowIndex][colIndex] = 'circle';
         }
+        setGameState('Processing');
         setBoardState(newBoardState);
-
-        if (currentPlayer === 'PlayerOne') {
-          setCurrentPlayer('PlayerTwo');
-        } else {
-          setCurrentPlayer('PlayerOne');
-        }
       }
     };
   };
+
+  const getWinningPlayer = () => {
+    if (currentPlayer === 'PlayerOne') {
+      return 'PlayerOneWin' as const;
+    }
+
+    return 'PlayerTwoWin' as const;
+  }
+
+  const checkForWin = () => {
+    // Check rows
+    for (const row of boardState) {
+      if (row[0] !== 'none' && row[0] === row[1] && row[1] === row[2]) {
+        return true;
+      }
+    }
+
+    // Check cols
+    for (let i = 0; i < boardState.length; i++) {
+      if (
+        boardState[0][i] !== 'none'
+        && boardState[0][i] === boardState[1][i]
+        && boardState[1][i] === boardState[2][i]
+      ) {
+        return true;
+      }
+    }
+
+    // Check diagonals
+    if (
+      (
+        boardState[0][0] !== 'none'
+        && boardState[0][0] === boardState[1][1]
+        && boardState[1][1] === boardState[2][2]
+      )
+      || (
+        boardState[0][2] !== 'none'
+        && boardState[0][2] === boardState[1][1]
+        && boardState[1][1] === boardState[2][0]
+      )
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  useEffect(() => {
+    if (gameState !== 'Processing') {
+      return;
+    }
+
+    if (checkForWin()) {
+      setGameState(getWinningPlayer());
+      return;
+    }
+
+    if (currentPlayer === 'PlayerOne') {
+      setCurrentPlayer('PlayerTwo');
+    } else {
+      setCurrentPlayer('PlayerOne');
+    }
+
+    setGameState('Playing');
+  }, [gameState, boardState]);
 
   return {
     boardState,
